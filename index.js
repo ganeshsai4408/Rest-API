f0bba7e3ea1833013bff46eaf548263cb841bc21
ganeshsai4408 <144612987+ganeshsai4408@users.noreply.github.com>
2025-05-25 00:39:09 +0530

    feat: enhance POST and PATCH routes with new features and add custom middleware

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 const express = require("express");
+const fs = require("fs");
 const users = require("./dummy_users.json");
+const e = require("express");
 const app = express();
 const PORT = 8000;
+app.use(express.urlencoded({ extended: false }));
+app.use((req, res, next) => {
+    fs.appendFile("./logs.txt", `${new Date().toISOString()} - ${req.method} ${req.url}\n`, (err) => {
+        if (err) {
+            console.error("Failed to write to log file:", err);
+        }
+        next();
+    });
+});
 //routes
 app.get("/user", (req, res) => {
     const html =`<ul>
@@ -18,14 +29,35 @@ app.get("/api/user/:id",(req,res)=>{
     res.send(user);
 })
 app.post("/api/user", (req, res) => {
-    res.json({ status:"pending"});
+
+    const body = req.body;
+    users.push({...body, id: users.length + 1});
+    fs.writeFileSync("./dummy_users.json", JSON.stringify(users, null, 2) ,(err,data)=>{
+
+        res.json({ status:"success", user: body });
+    });
+    console.log(body);
+
 
 });
 app.patch("/api/user", (req, res) => {
     res.json({ status:"pending"});
     
 });
-app.delete("/api/user", (req, res) => {
+app.delete("/api/user/:id", (req, res) => {
+    const id = req.params.id;
+    const userIndex = users.findIndex(user => user.id == id);
+     if (userIndex !== -1) {
+        users.splice(userIndex, 1);
+        fs.writeFile("./dummy_users.json", JSON.stringify(users, null, 2), (err) => {
+            if (err) {
+                return res.status(500).json({ status: "error", message: "Failed to update file" });
+            }
+            res.json({ status: "success", message: "User deleted" });
+        });
+    } else {
+        res.status(404).json({ status: "error", message: "User not found" });
+    }
     res.json({ status:"pending"});
     
 });
